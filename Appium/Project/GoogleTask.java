e07b2ebc2af6d5a28cc8c54ee37cccb94f4787da
ektasuman1 <ektasuman185@gmail.com>
2024-06-26 21:15:33 +0530

    Add files via upload

---
+++ Appium/Project/GoogleTask.java
@@ -0,0 +1,72 @@
+package projects;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GoogleTask {
+
+    AndroidDriver driver;
+
+    // Set up method
+    @BeforeClass
+    public void googleTasksSetup() throws MalformedURLException {
+        // Desired Capabilities
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.google.android.apps.tasks");
+        options.setAppActivity(".ui.TaskListsActivity");
+        options.noReset();
+
+        // Server Address
+        URL serverURL = new URL("http://127.0.0.1:4723/");
+
+        // Driver Initialization
+        driver = new AndroidDriver(serverURL, options);
+    }
+
+    @Test
+    public void addTasks() {
+        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
+        driver.findElement(AppiumBy.id("add_task_title")).sendKeys("Complete Activity with Google Tasks");
+        driver.findElement(AppiumBy.id("add_task_done")).click();
+
+        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
+        driver.findElement(AppiumBy.id("add_task_title")).sendKeys("Complete Activity with Google Keep");
+        driver.findElement(AppiumBy.id("add_task_done")).click();
+
+        driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
+        driver.findElement(AppiumBy.id("add_task_title")).sendKeys("Complete the second Activity Google Keep");
+        driver.findElement(AppiumBy.id("add_task_done")).click();
+
+
+        String  listdata1 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Complete Activity with Google Tasks\")")).getText();
+
+        String  listdata2 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Complete Activity with Google Keep\")")).getText();
+
+        String  listdata3 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Complete the second Activity Google Keep\")")).getText();
+
+
+        //Assertion
+
+        Assert.assertEquals(listdata1,"Complete Activity with Google Tasks");
+
+        Assert.assertEquals(listdata2,"Complete Activity with Google Keep");
+
+        Assert.assertEquals(listdata3,"Complete the second Activity Google Keep");
+
+
+    }
+}
+
